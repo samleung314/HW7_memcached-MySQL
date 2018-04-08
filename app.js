de652c988b9f984e8ecf027a132e7811ac10e944
samleung314 <samleung314@gmail.com>
2018-04-07 21:45:54 -0400

    /hw7 query

--- app.js
+++ app.js
@@ -51,3 +51,19 @@ var server = app.listen(port, function () {
 })
 
 module.exports = app;
+
+// CREATE TABLE assists (
+//     player VARCHAR(50) ,
+//     club VARCHAR(50) ,
+//     pos VARCHAR(50) ,
+//     gp INT ,
+//     gs INT ,
+//     a INT ,
+//     gwa INT ,
+//     hma INT ,
+//     rda INT ,
+//     a90 DECIMAL(2,2),
+//     PRIMARY KEY (player)
+// )
+
+//load data local infile 'assists.csv' into table assists fields terminated by ',' enclosed by '"' lines terminated by '\n' IGNORE 1 LINES (player, club, pos, gp, gs, a, gwa, hma, rda, a90);
